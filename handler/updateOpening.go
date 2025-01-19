667c8cf8e4eedefda32427b99045dc7ec8290265
Lucas Dantas da Cruz <ldantascruz@gmail.com>
2025-01-19 17:05:42 -0300

    feat: add update opening handler logic

--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -1,13 +1,73 @@
 package handler
 
 import (
-	"net/http"
-
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/ldantascruz/gopportunities/schemas"
+	"net/http"
 )
 
 func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "PUT Opening",
-	})
+	request := UpdateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+
+	// Save Opening
+	if err := db.Save(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error updating opening with id: %s", id))
+		return
+	}
+
+	response := schemas.OpeningResponse{
+		ID:        opening.ID,
+		CreatedAt: opening.CreatedAt,
+		UpdatedAt: opening.UpdatedAt,
+		DeletedAt: opening.DeletedAt.Time,
+		Role:      opening.Role,
+		Company:   opening.Company,
+		Location:  opening.Location,
+		Remote:    opening.Remote,
+		Link:      opening.Link,
+		Salary:    opening.Salary,
+	}
+
+	sendSuccess(ctx, "update-opening", response)
 }
