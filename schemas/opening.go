bd33d6dba1c94f50ac5de6819ea5077e0648574d
Lucas Dantas da Cruz <ldantascruz@gmail.com>
2025-01-19 17:07:15 -0300

    feat: add swagger configuration

--- schemas/opening.go
+++ schemas/opening.go
@@ -7,12 +7,12 @@ import (
 
 type Opening struct {
 	gorm.Model
-	Role     string
-	Company  string
-	Location string
-	Remote   bool
-	Link     string
-	Salary   int64
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   bool   `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
 }
 
 type OpeningResponse struct {
